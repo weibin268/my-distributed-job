142be20e5772d6396f34da8dd9725af4d1ccff0c
weibin268 <binweizhuang@hotmail.com>
2021-07-18 22:30:47 +0800

    aa

--- my-distributed-job-test/src/main/java/com/zhuang/distributedjob/job/TestJob.java
+++ my-distributed-job-test/src/main/java/com/zhuang/distributedjob/job/TestJob.java
@@ -3,12 +3,17 @@ package com.zhuang.distributedjob.job;
 import com.zhuang.distributedjob.annotation.Job;
 import com.zhuang.distributedjob.annotation.JobComponent;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @JobComponent
 public class TestJob {
 
-    @Job(cron = "0/1 * * * * ? ")
+    private Integer count = 0;
+
+    @Job(cron = "0/1 * * * * ?")//每秒执行1次
     public void test() {
-        System.out.println("test");
+        System.out.println("test -> " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
 
 }
