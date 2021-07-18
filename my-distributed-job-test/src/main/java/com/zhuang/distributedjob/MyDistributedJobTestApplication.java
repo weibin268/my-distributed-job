3d1517092e57d7eac8366f7fd265752b72e7ba51
weibin268 <binweizhuang@hotmail.com>
2021-07-18 21:28:41 +0800

    aa

---
+++ my-distributed-job-test/src/main/java/com/zhuang/distributedjob/MyDistributedJobTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhuang.distributedjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDistributedJobTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDistributedJobTestApplication.class, args);
+	}
+
+}
