8a7839db6ca325e150b95ccc9b0d1e7aa3914864
weibin268 <binweizhuang@hotmail.com>
2021-07-21 00:32:59 +0800

    aa

--- my-distributed-job-test/src/main/java/com/zhuang/distributedjob/job/TestJob.java
+++ my-distributed-job-test/src/main/java/com/zhuang/distributedjob/job/TestJob.java
@@ -14,8 +14,9 @@ public class TestJob {
     @Job(cron = "0/1 * * * * ?")    //每秒执行1次
     //@Job(cron = "0 10 0/1 * * ?")   //从00:10:00开始每隔1小时执行1次
     //@Job(cron = "0 0 2-6/1 * * ?")  //从02:00:00开始到06:00:00之间每隔1小时执行1次
-    //@Job(cron = "0 0 3 * * ?")      //每天凌晨03:00:00执行1次
-    //@Job(cron = "0 0 3,4 * * ?")    //每天凌晨03:00:00和04:00:00各执行1次
+    //@Job(cron = "0 0 3 * * ?")      //每天03:00:00执行1次
+    //@Job(cron = "0 0 3,4 * * ?")    //每天03:00:00和04:00:00各执行1次
+    //@Job(cron = "0 0 2 1 * ?")      //每月1日02:00:00执行1次
     public void test() {
         System.out.println("test -> " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " -> " + count++);
     }
